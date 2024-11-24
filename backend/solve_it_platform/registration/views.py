1ce38590c734977b117efaf7d5e52134d5d6f0c2
unknown <mrahamdjedcoach@gmail.com>
2024-11-24 14:50:26 +0100

    added registration app

---
+++ backend/solve_it_platform/registration/views.py
@@ -0,0 +1,87 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+from django.db import transaction
+from django.core.exceptions import ValidationError
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.forms import AuthenticationForm 
+from django.contrib.auth import login
+
+from .forms import TeamCreationForm,TeamForm,CreateUserForm
+
+from .models import Team,Participant
+
+
+
+
+def createTeamView(request:HttpRequest):
+    form = TeamCreationForm(request.POST or None)
+    err = None
+
+    if request.method == "POST" :
+        try :
+            with transaction.atomic():
+                if( form.is_valid() ):
+                    form.save()
+                    return render(request,"registration/createTeam.html",{ "err" : "DONE"})
+        except Exception as exp:
+            err = exp.__str__()
+    
+    context = {
+        "form" : form,        
+        "err" : err
+    }
+
+    return render(request,"registration/createTeam.html",context)
+
+
+
+def getTeam(teamForm:TeamForm):
+    try:
+        dbTeam = Team.objects.get(name=teamForm.data["teamName"])
+    except :    
+        raise ValidationError("Team didn't exist, maybe the team name is wrong")
+    if not check_password(teamForm.data["teamPassword"], dbTeam.password):
+        raise ValidationError("Team password is wrong")
+    return dbTeam
+
+def createParticipantView(request:HttpRequest):
+
+    userForm = CreateUserForm(request.POST or None)
+    teamForm = TeamForm(request.POST or None)
+    err = None
+
+    if request.method == "POST" :
+        try :
+            with transaction.atomic():
+                if( userForm.is_valid() ):
+                    teamObj = getTeam(teamForm)
+                    userObj = userForm.save()
+                    Participant(user=userObj, team=teamObj).save()
+                    return render(request,"registration/createParticipant.html",{ "err" : "DONE"})
+        except Exception as exp:
+            err = exp.__str__()[2:-2]
+    
+    context = {
+        "teamForm" : teamForm,        
+        "userForm" : userForm,        
+        "err" : err
+    }
+
+    return render(request,"registration/createParticipant.html",context)
+
+
+def participantLoginView(request : HttpRequest):
+    form = AuthenticationForm(request,request.POST or None)
+    if request.method == "POST" :
+        if( form.is_valid() ):
+            login(request, form.get_user())
+            context = {
+                "form" : form,
+                "status" : "Succed",
+            }
+            return render(request,"registration/participantLogin.html",context)
+    context = {
+        "form" : form,
+        "status" : "loading",
+    }
+    return render(request,"registration/participantLogin.html",context)
--- backend/solve_it_platform/solve_it_platform/settings.py
+++ backend/solve_it_platform/solve_it_platform/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # ! OUR APPS 
+    'registration.apps.RegistrationConfig',
 ]
 
 MIDDLEWARE = [
