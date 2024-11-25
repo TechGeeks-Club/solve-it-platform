84dc5cc20c5ba21e6ad209ff389617aaa8de72f1
0x03bb <alibouakaz91@gmail.com>
2024-11-25 16:39:05 +0100

    Add| upload method, Fix|admin test

--- src/tasks/admin.py
+++ src/tasks/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
 from .models import Phase, Category, Task, TaskTest, TaskSolution
 
 admin.site.register(Phase)
 admin.site.register(Category)
-admin.site.register(Task)
+# admin.site.register(Task)
 admin.site.register(TaskTest)
 admin.site.register(TaskSolution)
+
+@admin.register(Task)
+class TaskAdmin(ModelAdmin):
+    
+    model = Task
+    list_display = ('_title','phase','level','category','points')
+    list_display_links = list_display
+    
+    search_fields = ('_title',)
+    list_filter = ('phase', 'level', 'category')
+    
+    
+    def _title(self, obj):
+        if len(obj.title) <= 15:
+            return obj.title
+        return obj.title[:15] + "..."
+    
+# @admin.register(TaskSolution)
+# class TaskSolutionAdmin(ModelAdmin):
+    
+#     model = TaskSolution
+#     list_display = ('_title','phase','level','category','points')
+#     list_display_links = list_display
+    
+#     search_fields = ('_title',)
+#     list_filter = ('phase', 'level', 'category')
+    
+    
+#     def _title(self, obj):
+#         if len(obj.title) <= 15:
+#             return obj.title
+#         return obj.title[:15] + "..."
+    
--- src/tasks/models.py
+++ src/tasks/models.py
@@ -42,11 +42,19 @@ class TaskTest(models.Model):
     def __str__(self):
         return self.task.title + " tests"
 
+
+
+
+def get_file_path(participant, filename):
+    file_extention = filename.split('.')[-1]
+    
+    return f'upload/{participant.team.name}/{filename}_{participant.id}.c'
+
 class TaskSolution(models.Model):
     task = models.ForeignKey(Task, null=False, on_delete=models.CASCADE)
     participant = models.ForeignKey(Participant, null=True, on_delete=models.SET_NULL)
     team = models.ForeignKey(Team, null=False, on_delete=models.CASCADE)
-    code = models.TextField(null=False)
+    code = models.FileField(upload_to=get_file_path, blank=True, max_length=100)
     score = models.IntegerField(null=False)
     tries = models.IntegerField(null=False, default=0, validators=[MaxValueValidator(3)] )
     
