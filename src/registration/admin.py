0d92f62f85cb8492ab273f3d4ac7620ee9e76d3a
0x03bb <alibouakaz91@gmail.com>
2024-11-26 12:13:44 +0100

    add assets

--- src/registration/admin.py
+++ src/registration/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
 from .models import Team,Participant
 
 admin.site.register(Team)
-admin.site.register(Participant)
+# admin.site.register(Participant)
+
+class ParticipantAdmin(ModelAdmin):
+    
+    model = Participant
+    list_display = ('name')
+    list_display_links = list_display
+    
+    search_fields = ('_title',)
+    list_filter = ('phase', 'level', 'category')
+    
+    
+    def _title(self, obj):
+        if len(obj.title) <= 15:
+            return obj.title
+        return obj.title[:15] + "..."
