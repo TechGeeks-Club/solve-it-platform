378a6255c8de0aa6745f773d9c74a77d08c51b58
unknown <mrahamdjedcoach@gmail.com>
2024-11-30 14:10:00 +0100

    first version of templates branch, waiting for new frontend updates

--- src/src/settings.py
+++ src/src/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+LOGIN_URL = '/participant_login'
 # Application definition
 
 INSTALLED_APPS = [
--- src/tasks/views.py
+++ src/tasks/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.db import transaction
 from django.db.models import Prefetch
+from django.contrib.auth.decorators import login_required
+
 
 from .models import Phase,Task,TaskSolution,TaskTest
 from registration.models import Participant
 
 
-
+@login_required
 def tasksDisplayView(request:HttpRequest):
     phases = Phase.objects.prefetch_related("phase_tasks","phase_tasks__category")
     context = {
@@ -22,56 +24,45 @@ def checkParticipationExistance(task:Task, participant:Participant):
         return solutionObj
     except :
         return None
-        
 
+
+@login_required
 def taskView(request:HttpRequest, task_id:int):
+    
     task_tests_query = TaskTest.objects.filter(display=True)
     taskObj = Task.objects.prefetch_related(Prefetch('task_tests', queryset=task_tests_query)).get(id=task_id)
-    solutionObj = None
-    try:
-        solutionObj = TaskSolution.objects.get(task=taskObj)
-    except :
-        pass
+
+    participantObj = Participant.objects.get(user = request.user)
+    solutionObj = checkParticipationExistance(taskObj,participantObj)
 
     context = {
         "task" : taskObj,
         "solution" : solutionObj,
     }
+    if request.method == "POST" :
+        print(f" [[ For Debug : {request.FILES} ]] ")
 
     return render(request,"tasks/challenge-detailes.html",context)
     # ! This work is not ready yet
 
     # if request.method == "POST" :
-    #     # ? i don't want the access to this page to require authentication, just the code submition
-    #     if request.user.is_authenticated :
-    #         try :
-    #             with transaction.atomic():
-                    
-    #                 participantObj = Participant.objects.get(user = request.user)
-    #                 # ? to see if it participate before in that task 
-    #                 solutionObj = checkParticipationExistance(taskObj,participantObj)
-                    
-    #                 if solutionObj is not None :
-    #                     if solutionObj.tries < 3 :
-                            
-    #                         # ! HERE CODE PART
-    #                         solutionObj.code = 
-    #                         solutionObj.tries = solutionObj.tries + 1
-    #                         solutionObj.save()
-    #                     else :
-    #                         err = "You already use all your chances"
-    #                 else :
-    #                     TaskSolution(
-    #                         task= taskObj,
-    #                         participant=participantObj,
-    #                         team=participantObj.team,
-    #                         code= #! HERE CODE PART
-    #                     ).save()
-
-    #                     # TaskSolutionForm.save(task=taskObj ,participant=participantObj)
-            
-    #         except Exception as exp :
-    #             err = exp.__str__()
-    #     else :
-    #         err = "User is not authenticated"
+#         try :
+#             with transaction.atomic():
+                    #? it means they never paticipate in this task
+#                 if solutionObj is not None :
+#                     if solutionObj.tries < 3 :
+#                         solutionObj.code = # ! HERE CODE PART 
+#                         solutionObj.tries = solutionObj.tries + 1
+#                         solutionObj.save()
+#                     else :
+#                         err = "You already use all your chances"
+#                 else :
+#                     TaskSolution(
+#                         task= taskObj,
+#                         participant=participantObj,
+#                         team=participantObj.team,
+#                         code= #! HERE CODE PART
+#                     ).save(
+#         except Exception as exp :
+#             err = exp.__str__()
 
