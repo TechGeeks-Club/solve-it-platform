49960532a67fdff1114de2c20c9de3fba6b98159
B-AJ-Amar <amarbouakaz91@gmail.com>
2024-12-04 00:04:05 +0100

    fix : db

--- src/src/settings.py
+++ src/src/settings.py
@@ -77,25 +77,25 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': "SolveItDB",
-#         'USER': "SolveItDB_owner",
-#         'PASSWORD': "v9g6CLEFscjk",
-#         'HOST': "ep-plain-water-a25evwjr.eu-central-1.aws.neon.tech",
-#         'PORT': 5432,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "SolveItDB",
+        'USER': "SolveItDB_owner",
+        'PASSWORD': "v9g6CLEFscjk",
+        'HOST': "ep-plain-water-a25evwjr.eu-central-1.aws.neon.tech",
+        'PORT': 5432,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
