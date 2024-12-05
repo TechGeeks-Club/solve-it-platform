a9b18ee2f175158a7f4c924c10e1c6cc13845686
B-AJ-Amar <amarbouakaz91@gmail.com>
2024-12-05 13:16:46 +0100

    some changes

--- src/src/settings.py
+++ src/src/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@9al=@0xvy#m_)%8q8m=e^b3k+@-mki*zs(ch0c6d%9!l+!cy5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [".vercel.app","localhost","127.0.0.1" , "*"]
+ALLOWED_HOSTS = [".univ-guelma.dz","localhost","127.0.0.1" , "10.10.10.103"]
 
 LOGIN_URL = '/login'
 # Application definition
